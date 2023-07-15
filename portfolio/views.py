9d57f22099f9c9fd01587d42f65cabf581819427
Delroy Brown <delroybrown.db@gmail.com>
2023-07-15 16:30:58 +0100

    Updated blog area

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,12 +2,7 @@ import json
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from .models import (Post,
-                     Experience,
-                     Skills,
-                     Certificates,
-                     Message
-                     )
+from .models import Post, Experience, Skills, Certificates, Message
 
 
 def home(request):
@@ -22,17 +17,24 @@ def home(request):
 
 def post_detail(request, id):
     post = Post.objects.get(id=id)
-    return render(request, "blog-single-1.html", {"post": post})
+    next_post = Post.objects.filter(id__gt=post.id).order_by("id").first()
+    previous_post = Post.objects.filter(id__lt=post.id).order_by('-id').first()
+    context = {
+        "post" : post,
+        "next_post" : next_post,
+        "previous_post" : previous_post,
+    }
+    return render(request, "blog-single-1.html", context)
 
 
 @csrf_exempt
 def send_email(request):
     if request.method == "POST":
         data = json.loads(request.body)
-        name = data.get('userName')
-        email = data.get('userEmail')
-        subject = data.get('subject')
-        content = data.get('content')
+        name = data.get("userName")
+        email = data.get("userEmail")
+        subject = data.get("subject")
+        content = data.get("content")
 
         Message.objects.create(name=name, email=email, subject=subject, content=content)
 
