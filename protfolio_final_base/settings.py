d2aec740fc8e5811141229ec3ce006d3c8c41ef3
Delroy Brown <delroybrown.db@gmail.com>
2023-09-09 22:27:01 +0100

    Updated, links to projects

--- protfolio_final_base/settings.py
+++ protfolio_final_base/settings.py
@@ -111,25 +111,25 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 
-# if "USE_AWS" in os.environ:
-#     # Cache Control
-#     AWS_S3_OBJECT_PARAMETERS = {
-#         # 'Expires': 'Thu, 31 2099 20:00:00 GMT',
-#         "CacheControl": "max-age=94608000",
-#     }
-#     AWS_STORAGE_BUCKET_NAME = "delroybrown-portfolio"
-#     AWS_S3_REGION_NAME = "eu-west-2"
-#     AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
-#     AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
-#     AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+if "USE_AWS" in os.environ:
+    # Cache Control
+    AWS_S3_OBJECT_PARAMETERS = {
+        # 'Expires': 'Thu, 31 2099 20:00:00 GMT',
+        "CacheControl": "max-age=94608000",
+    }
+    AWS_STORAGE_BUCKET_NAME = "delroybrown-portfolio"
+    AWS_S3_REGION_NAME = "eu-west-2"
+    AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 
-#     STATICFILES_STORAGE = "custom_storages.StaticStorage"
-#     STATICFILES_LOCATION = "static/"
-#     DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
-#     MEDIAFILES_LOCATION = "media/"
+    STATICFILES_STORAGE = "custom_storages.StaticStorage"
+    STATICFILES_LOCATION = "static/"
+    DEFAULT_FILE_STORAGE = "custom_storages.MediaStorage"
+    MEDIAFILES_LOCATION = "media/"
 
-#     STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
-#     MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
+    STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/"
+    MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/"
 
 
 # Default primary key field type
